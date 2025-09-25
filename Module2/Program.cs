5ac3dbc3f4202f13516af54e2989ab496867bad3
Scott Dyrness <scottdyrness@gmail.com>
2025-09-24 19:34:23 -0700

    created list of type students, 4 instances of type student, added 4 students to list students.

--- Module2/Program.cs
+++ Module2/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var students = new List<Student>();
+        
+        var student1 = new Student();
+        student1.Name = "Scott";
+        student1.ID = 1;
+        students.Add(student1);
+        
+        var student2 = new Student();
+        student2.Name = "Zion";
+        student2.ID = 2;
+        students.Add(student2);
+        
+        var student3 = new Student();
+        student3.Name = "Mercy";
+        student3.ID = 3;
+        students.Add(student3);
+        
+        var student4 = new Student();
+        student4.Name = "Maverick";
+        student4.ID = 4;
+        students.Add(student4);
+        
     }
 }
