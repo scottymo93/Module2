b60f2e805bf7a619548994c190eb13cd9c4e62b6
Scott Dyrness <scottdyrness@gmail.com>
2025-09-24 19:17:00 -0700

    Created Student Class, Method AddGrade that accepts a double, Method Addgrade that accepts double[].

---
+++ Module2/Student.cs
@@ -0,0 +1,18 @@
+namespace Module2;
+
+public class Student
+{
+    public string Name { get; set; }
+    public int ID { get; set; }
+    public List<double> Grades { get; set; } = new List<double>();
+
+    public void AddGrade(double grade)
+    {
+        Grades.Add(grade);
+    }
+
+    public void AddGrade(params double[] grades)
+    {
+        Grades.AddRange(grades);
+    }
+}
